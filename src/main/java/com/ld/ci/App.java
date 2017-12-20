fe5229c7265091d8c03e6f08b80e1264156a56c5
lexusdrivers <lexus.drivers.aws@gmail.com>
2017-12-20 13:14:08 -0800

    checking jenkins branch

--- src/main/java/com/ld/ci/App.java
+++ src/main/java/com/ld/ci/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
     	//Testing again
-    	
+    	System.out.println("jenkins testing");
     	//This is only a test
         System.out.println( "Hello World!" );
     }
